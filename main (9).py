ccc0eeabb19d897125a01e54f5586f0f1797e431
Pyksik <158447945+Pyksik@users.noreply.github.com>
2024-03-21 11:47:39 +0300

    задачи -( 24-28.) 21.03.24

--- main (9).py
+++ main (9).py
@@ -1,9 +1,11 @@
-z=9
-w=5
-if z > w:
-    e = z - w
-elif z < w:
-    e = z + w 
-else:
-    e = z
-print(e)
+
+M = 4
+N = 3
+B = [[1, 0, 2],
+     [0, 3, 4],
+     [5, 0, 6],
+     [0, 0, 7]]
+for x in B:
+    print(x)
+egor = sum(1 for x in B for elem in x if elem != 0)
+print(f"Количество ненулевых элементов в матрице: {egor}")
